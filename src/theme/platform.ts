8e68bc42780ff58c8ed388c473c8bfb194902edc
wangcch <ci713@qq.com>
2019-11-26 23:24:06 +0800

    feat: platform theme

--- src/theme/platform.ts
+++ src/theme/platform.ts
@@ -5,7 +5,11 @@ import AntdThemeVariables from "../types/variables";
  * antd variables: https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
  */
 const platform: AntdThemeVariables = {
-  "@border-radius-base": "0",
+  "@font-size-base": "12px",
+  "@primary-color": "#3674FF",
+  "@border-color-base": "#E8E8E8",
+  "@border-radius-base": "2px",
+  "@box-shadow-base": "0 2px 8px rgba(0, 0, 0, 0.1)",
 };
 
 export default platform;
