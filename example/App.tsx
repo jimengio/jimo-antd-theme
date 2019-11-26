3302086c8d9dd98bc9a945abce39daf38dbf6da5
wangcch <ci713@qq.com>
2019-11-26 23:25:53 +0800

    docs(example): multi themes preview

--- example/App.tsx
+++ example/App.tsx
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { css } from "emotion";
 
-import { Button } from "antd";
+import { Radio } from "antd";
+import AntdComponents from "./components";
 
-interface AppProps {}
+import platform from "./theme/platform";
 
-export default function App(props: AppProps) {
+export default function App() {
+  const [theme, setTheme] = useState("default");
+
+  const styleRef = useRef<HTMLStyleElement | null>();
+
+  useEffect(() => {
+    styleRef.current = document.querySelector<HTMLStyleElement>(
+      `style[jimo-antd-theme=""]`
+    );
+
+    if (!styleRef.current) {
+      styleRef.current = document.createElement("style");
+    }
+
+    styleRef.current.setAttribute("jimo-antd-theme", "");
+    styleRef.current.innerText = theme === "platform" ? platform : "";
+    document.head.appendChild(styleRef.current);
+  }, [theme]);
+
   return (
     <div className={styleApp}>
-      <Button>Button</Button>
+      <Radio.Group
+        buttonStyle="solid"
+        value={theme}
+        onChange={e => setTheme(e.target.value)}
+      >
+        <Radio.Button value="default">Antd Default</Radio.Button>
+        <Radio.Button value="platform">Platform</Radio.Button>
+      </Radio.Group>
+      <div style={{ marginTop: 16 }}>
+        <AntdComponents />
+      </div>
     </div>
   );
 }
 
 const styleApp = css`
-  text-align: center;
+  padding: 16px;
 `;
---
+++ example/components.tsx
@@ -0,0 +1,225 @@
+import React, { FC } from "react";
+
+import {
+  Button,
+  Checkbox,
+  DatePicker,
+  Input,
+  InputNumber,
+  Rate,
+  Radio,
+  Switch,
+  Slider,
+  Select,
+  TimePicker,
+  Tag,
+  Tabs,
+  Table,
+  Alert,
+  Progress,
+  Row,
+  Col,
+} from "antd";
+import { css } from "emotion";
+
+const PreCard: FC<{ title?: string }> = props => {
+  const { title, children } = props;
+
+  return (
+    <div className={styleCard}>
+      <h2>{title}</h2>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+const AntdComponents: FC = () => {
+  return (
+    <Row>
+      <Col xs={24} md={12} lg={8}>
+        <PreCard title={"Button"}>
+          <Button type="primary">Primary</Button>
+          <Button>Default</Button>
+          <Button type="dashed">Dashed</Button>
+          <Button type="danger">Danger</Button>
+          <Button type="link">Link</Button>
+          <br />
+          <Button type="primary" icon="download" />
+          <Button type="primary" shape="circle" icon="download" />
+          <Button type="primary" shape="round" icon="download" />
+          <Button type="primary" shape="round" icon="download">
+            Download
+          </Button>
+          <Button type="primary" icon="download">
+            Download
+          </Button>
+          <br />
+          <Button.Group>
+            <Button type="primary">Backward</Button>
+            <Button type="primary">Forward</Button>
+          </Button.Group>
+        </PreCard>
+      </Col>
+      <Col xs={24} md={12} lg={8}>
+        <PreCard title={"Checkbox"}>
+          <Checkbox>Checkbox</Checkbox>
+          <Checkbox indeterminate>indeterminate</Checkbox>
+        </PreCard>
+        <PreCard title={"Radio"}>
+          <Radio.Group name="radiogroup" defaultValue={1}>
+            <Radio value={1}>A</Radio>
+            <Radio value={2}>B</Radio>
+            <Radio value={3}>C</Radio>
+          </Radio.Group>
+          <Radio.Group defaultValue="a" buttonStyle="solid">
+            <Radio.Button value="a">Hangzhou</Radio.Button>
+            <Radio.Button value="b">Shanghai</Radio.Button>
+            <Radio.Button value="c">Beijing</Radio.Button>
+          </Radio.Group>
+        </PreCard>
+        <PreCard title={"Switch"}>
+          <Switch defaultChecked />
+        </PreCard>
+      </Col>
+      <Col xs={24} md={12} lg={8}>
+        <PreCard title={"DatePicker"}>
+          <DatePicker />
+          <br />
+          <DatePicker.MonthPicker placeholder="Select month" />
+          <br />
+          <DatePicker.RangePicker />
+          <br />
+          <DatePicker.WeekPicker placeholder="Select week" />
+        </PreCard>
+        <PreCard title={"TimePicker"}>
+          <TimePicker />
+        </PreCard>
+      </Col>
+      <PreCard title={"InputNumber"}>
+        <InputNumber />
+      </PreCard>
+      <Col xs={24} md={12} lg={8}>
+        <PreCard title={"Input"}>
+          <Input />
+          <Input.TextArea />
+          <Input.Group compact>
+            <Input style={{ width: "20%" }} defaultValue="0571" />
+            <Input style={{ width: "30%" }} defaultValue="26888888" />
+          </Input.Group>
+        </PreCard>
+        <PreCard>
+          <Select defaultValue="lucy" style={{ width: 120 }}>
+            <Select.Option value="jack">Jack</Select.Option>
+            <Select.Option value="lucy">Lucy</Select.Option>
+            <Select.Option value="disabled" disabled>
+              Disabled
+            </Select.Option>
+          </Select>
+          <br />
+          <Select defaultValue="lucy" mode={"multiple"} style={{ width: 320 }}>
+            <Select.Option value="jack">Jack</Select.Option>
+            <Select.Option value="lucy">Lucy</Select.Option>
+            <Select.Option value="disabled" disabled>
+              Disabled
+            </Select.Option>
+          </Select>
+        </PreCard>
+      </Col>
+      <Col xs={24} md={12} lg={8}>
+        <PreCard title={"Rate"}>
+          <Rate />
+        </PreCard>
+        <PreCard title={"Slider"}>
+          <Slider defaultValue={20} />
+        </PreCard>
+        <PreCard title={"Progress"}>
+          <Progress percent={30} status="active" />
+          <Progress percent={50} />
+          <Progress percent={70} status="exception" />
+          <Progress percent={100} />
+        </PreCard>
+        <PreCard title={"Tag"}>
+          <Tag>Tag 1</Tag>
+          <Tag closable>Tag 2</Tag>
+        </PreCard>
+      </Col>
+      <Col xs={24} md={12} lg={8}>
+        <PreCard title={"Alert"}>
+          <Alert message="Success Text" type="success" />
+          <Alert message="Info Text" type="info" />
+          <Alert message="Warning Text" type="warning" />
+          <Alert message="Error Text" type="error" />
+        </PreCard>
+      </Col>
+      <Col lg={24}>
+        <PreCard title={"Tabs"}>
+          <Tabs defaultActiveKey="1">
+            <Tabs.TabPane tab="Tab 1" key="1">
+              Content of Tab Pane 1
+            </Tabs.TabPane>
+            <Tabs.TabPane tab="Tab 2" key="2">
+              Content of Tab Pane 2
+            </Tabs.TabPane>
+            <Tabs.TabPane tab="Tab 3" key="3">
+              Content of Tab Pane 3
+            </Tabs.TabPane>
+          </Tabs>
+        </PreCard>
+      </Col>
+      <Col lg={24}>
+        <PreCard title={"Table"}>
+          <Table
+            dataSource={[
+              {
+                key: "1",
+                name: "John Brown",
+                age: 32,
+                address: "New York No. 1 Lake Park",
+              },
+              {
+                key: "2",
+                name: "Jim Green",
+                age: 42,
+                address: "London No. 1 Lake Park",
+              },
+              {
+                key: "3",
+                name: "Joe Black",
+                age: 32,
+                address: "Sidney No. 1 Lake Park",
+              },
+            ]}
+          >
+            <Table.Column title="Name" dataIndex="name" key="lastName" />
+            <Table.Column title="Age" dataIndex="age" key="age" />
+            <Table.Column title="Address" dataIndex="address" key="address" />
+          </Table>
+        </PreCard>
+      </Col>
+    </Row>
+  );
+};
+
+export default AntdComponents;
+
+const styleCard = css`
+  padding: 8px;
+
+  .ant-btn,
+  .ant-calendar-picker,
+  .ant-input,
+  .ant-switch,
+  .ant-select,
+  .ant-radio-group {
+    margin-right: 8px;
+    margin-bottom: 8px;
+  }
+
+  .ant-alert {
+    margin-bottom: 8px;
+  }
+
+  .ant-btn-group > .ant-btn {
+    margin-right: 0;
+  }
+`;
