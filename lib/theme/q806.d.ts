3662ff338c3e544a4d823812a8c6e9c42d35c322
wangcch <ci713@qq.com>
2020-03-16 16:07:01 +0800

    feat(theme): new platform and q806

---
+++ lib/theme/q806.d.ts
@@ -0,0 +1,8 @@
+import AntdThemeVariablesV3 from "../types/variables_v3";
+/**
+ * q806
+ *
+ * antd variables: https://raw.githubusercontent.com/ant-design/ant-design/3.26.13/components/style/themes/default.less
+ */
+declare const q806: AntdThemeVariablesV3;
+export default q806;
---
+++ src/theme/q806.ts
@@ -0,0 +1,15 @@
+import AntdThemeVariablesV3 from "../types/variables_v3";
+/**
+ * q806
+ *
+ * antd variables: https://raw.githubusercontent.com/ant-design/ant-design/3.26.13/components/style/themes/default.less
+ */
+const q806: AntdThemeVariablesV3 = {
+  "@font-size-base": "12px",
+  "@primary-color": "#3674FF",
+  "@border-color-base": "#E8E8E8",
+  "@border-radius-base": "2px",
+  "@box-shadow-base": "0 2px 8px rgba(0, 0, 0, 0.1)",
+};
+
+export default q806;
