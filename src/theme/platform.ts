8d75ffc0a68d17b66e295272e929468db9afd2bd
wangcch <ci713@qq.com>
2020-03-20 10:29:58 +0800

    feat(theme): upgrade platform
    text-color, table-padding …

--- src/theme/platform.ts
+++ src/theme/platform.ts
@@ -6,6 +6,8 @@ import AntdThemeVariables from "../types/variables";
  */
 const platform: AntdThemeVariables = {
   "@font-size-base": "14px",
+  // text
+  "@text-color": "#323232",
   // colors
   "@primary-color": "#3674FF",
   "@success-color": "#4DAD1E",
@@ -14,11 +16,13 @@ const platform: AntdThemeVariables = {
   "@border-color-base": "#E8E8E8",
   "@border-radius-base": "2px",
   "@box-shadow-base": "0 2px 8px rgba(0, 0, 0, 0.1)",
+  // tag
   "@tag-font-size": "@font-size-base",
-  "@progress-radius": "2px",
+  // progress
+  "@progress-radius": "0",
   // Table
   "@table-header-bg": "#F2F2F2",
-  "@table-padding-vertical": "12px",
+  "@table-padding-vertical": "9px",
   // Tabs
   "@tabs-horizontal-margin": "0 16px 0 0",
 };
