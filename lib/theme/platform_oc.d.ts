f21fcd8b423c59287587e3e74bd1335a80d26313
岳磊 <yueleiMaster@gmail.com>
2021-03-05 14:40:51 +0800

    添加platform-oc主题

---
+++ lib/theme/platform_oc.d.ts
@@ -0,0 +1,8 @@
+import AntdThemeVariables from "../types/variables";
+/**
+ * platform_oc
+ * 目前用于平台订单中心应用
+ * antd variables: https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+ */
+declare const platform: AntdThemeVariables;
+export default platform;
---
+++ src/theme/platform_oc.ts
@@ -0,0 +1,39 @@
+import AntdThemeVariables from "../types/variables";
+/**
+ * platform_oc
+ * 目前用于平台订单中心应用
+ * antd variables: https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+ */
+const platform: AntdThemeVariables = {
+  "@font-size-base": "14px",
+  // text
+  "@text-color": "#2F3B4C",
+  // colors
+  "@primary-color": "#418BFF",
+  "@success-color": "#4DAD1E",
+  "@error-color": "#F5222D",
+  "@warning-color": "#FFC161",
+  "@border-color-base": "#DEDEDE",
+  "@border-radius-base": "2px",
+  "@box-shadow-base": "0 2px 8px rgba(0, 0, 0, 0.1)",
+  "@primary-5": "@primary-color",
+  "@input-placeholder-color": "#9C9C9C",
+  // tag
+  "@tag-font-size": "@font-size-base",
+  // progress
+  "@progress-radius": "0",
+  // Table
+  "@table-header-bg": "#D8E7FF",
+  "@table-header-color": "#2F3B4C",
+  "@table-padding-vertical": "9px",
+  "@table-row-hover-bg": "#EFF5FF",
+  "@table-expanded-row-bg": "#F8F8F8",
+  // Tabs
+  "@tabs-horizontal-margin": "0 16px 0 0",
+  // Form
+  "@label-color": "#2F3B4C",
+  // Btn
+  "@btn-height-base": "36px",
+};
+
+export default platform;
